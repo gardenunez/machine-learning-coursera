02887db2a94c29bdee0ce17a20015c718d9e6487
jorge jardines <gardenunez@gmail.com>
2016-03-20 01:42:17 +0100

    plotting the data

--- plotData.m
+++ plotData.m
@@ -15,6 +15,10 @@ function plotData(x, y)
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
 figure; % open a new figure window
+plot(x, y, 'rx', 'MarkerSize', 10);       %plot the data
+ylabel('Profit in $10,000s');             %set y-axis label
+xlabel('Population of City in 10,000s');  %set x-axis label
+
 
 
 
